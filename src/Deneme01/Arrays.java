5581c760b5d432b789feb60890e65f393027b5f0
AbdulkerimMutlu <a.kerim1990@gmail.com>
2024-08-21 23:14:34 +0300

    Initial commit

---
+++ src/Deneme01/Arrays.java
@@ -0,0 +1,18 @@
+package Deneme01;
+
+import java.util.ArrayList;
+
+public class Arrays {
+    public static void main(String[] args) {
+        ArrayList<String>bismil=new ArrayList<>();
+        bismil.add("Kerim");
+        bismil.add("Sefa");
+        bismil.add("Merve");
+        bismil.add("Ümmühan");
+        for (int i = 0; i < bismil.size(); i++) {
+            System.out.println("bismil = " + bismil.get(i));
+        }
+        boolean VarMi=bismil.contains("Sefa");
+        System.out.println("bismil = " + VarMi);
+    }
+}
